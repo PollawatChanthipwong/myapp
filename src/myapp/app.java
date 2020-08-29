8c003cd59de01899ca3ccf391115bffdb4d3133b
Gun <61050860@kmitl.ac.th>
2020-08-30 00:55:45 +0700

    bring back app.java from commit 4f27535

--- src/myapp/app.java
+++ src/myapp/app.java
@@ -2,6 +2,6 @@ package myapp;
 
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello animal!");
+        System.out.println("Hello World!");
     }
 }
